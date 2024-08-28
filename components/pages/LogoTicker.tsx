93954ebc826388fb00e53a90a95d76745985dc48
sardorazimov <sardorazimov2901@gmail.com>
2024-08-28 22:14:54 +0300

    TimeLine

--- components/pages/LogoTicker.tsx
+++ components/pages/LogoTicker.tsx
@@ -1,20 +1,9 @@
 'use client'
-import acmeLogo from "@/public/assets/images/acme.png";
-import quantumLogo from "@/public/assets/images/quantum.png";
-import echoLogo from "@/public/assets/images/echo.png";
-import celestialLogo from "@/public/assets/images/celestial.png";
-import pulseLogo from "@/public/assets/images/pulse.png";
-import apexLogo from "@/public/assets/images/apex.png";
+
+import { images } from "@/types/logotriker";
 import Image from "next/image";
 
-const images = [
-  { src: acmeLogo, alt: "Acme Logo" },
-  { src: quantumLogo, alt: "Quantum Logo" },
-  { src: echoLogo, alt: "Echo Logo" },
-  { src: celestialLogo, alt: "Celestial Logo" },
-  { src: pulseLogo, alt: "Pulse Logo" },
-  { src: apexLogo, alt: "Apex Logo" },
-];
+
 
 export const LogoTicker = ({
  
@@ -22,7 +11,7 @@ export const LogoTicker = ({
 
 }) => {
   return (
-    <section className="mt-8 py-16 sm:py-24 ">
+    <div className="mt-8 py-16 sm:py-24 ">
       <div className="container">
         <h2 className="text-xl text-center text-white/70">
           Animated Logo Styles Linear Gradient
@@ -34,17 +23,17 @@ export const LogoTicker = ({
           after:bg-[linear-gradinet(to_right,#000,rgb(0,0,0,0))]
         ">
          <div className="flex gap-16">
-         {images.map((imageSrc) => (
-           <Image
-            
-            src={imageSrc.src}
-            alt={imageSrc.alt}
-            className="flex-none h-8 w-auto"
+           {images.map((img, index) => (
+            <Image 
+             key={index}
+             src={img.src}
+             alt={img.alt}
+             className="h-11 w-auto flex-none"
             />
-         ))}
+           ))}
         </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
---
+++ types/logotriker.ts
@@ -0,0 +1,30 @@
+"use clinet"
+import acmeLogo from "@/public/assets/images/acme.png";
+import quantumLogo from "@/public/assets/images/quantum.png";
+import echoLogo from "@/public/assets/images/echo.png";
+import celestialLogo from "@/public/assets/images/celestial.png";
+import pulseLogo from "@/public/assets/images/pulse.png";
+import apexLogo from "@/public/assets/images/apex.png";
+
+export const images = [
+    { 
+        src: acmeLogo,
+        alt: "Acme Logo" 
+    },
+    { 
+        src: quantumLogo, 
+        alt: "Quantum Logo" },
+    { 
+        src: echoLogo, 
+        alt: "Echo Logo" },
+    { 
+        src: celestialLogo, 
+        alt: "Celestial Logo" },
+    { 
+        src: pulseLogo, 
+        alt: "Pulse Logo" },
+    { 
+        src: apexLogo, 
+        alt: "Apex Logo" },
+  ];
+  
